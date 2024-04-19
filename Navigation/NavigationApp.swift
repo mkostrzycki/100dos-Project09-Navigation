4e02103c6640fe78bdf41d3d3771f3d1293b71fa
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-19 13:22:38 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by MaćKo on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
